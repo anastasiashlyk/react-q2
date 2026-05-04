0da09b02043b81f3dc385a4359bf2826e7b1920f
Anastasia Shlyk <shlykk.anastasia@gmail.com>
2026-05-04 21:51:08 +0200

    feat: add cards

---
+++ rs-react-app/src/components/CardsList/Card.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { CardInfo } from '../../types/pokeapi_types';
+import "./index.css"
+
+type CardProps = {
+  info: CardInfo;
+}
+
+class Card extends React.Component<CardProps>{
+
+  render(){
+    return (
+      <div className='card'>
+        {this.props.info.image && (<img src={this.props.info.image} alt={this.props.info.name} />)}
+        <h3>Name: {this.props.info.name}</h3>
+        <p>Height: {this.props.info.height}</p>
+        <p>Weight: {this.props.info.weight}</p>
+      </div>
+    )
+  }
+}
+
+export default Card;
