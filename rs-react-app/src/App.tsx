f14a0ea0d30bbe48ead9f429301eebce63988c7c
Anastasia Shlyk <shlykk.anastasia@gmail.com>
2026-05-04 21:06:12 +0200

    fix: first version of app

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,122 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import React from 'react'
+import SearchBar from './components/SearchBar/SearchBar'
+import CardsList from './components/CardsList/CardsList'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { CardInfo } from './types/pokeapi_types'
+import {getPokemonByName, getPokemonList} from './api/requests'
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+import { SEARCH_KEY } from './constants/consts'
 
-      <div className="ticks"></div>
+interface State {
+  results: CardInfo[];
+  searchTerm: string;
+  isLoading: boolean;
+  error: string | null;
+}
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+
+class App extends React.Component<object, State> {
+  state: State = {
+    results: [],
+    searchTerm: localStorage.getItem(SEARCH_KEY) ?? '',
+    isLoading: false,
+    error: null
+  }
+
+  async componentDidMount(): Promise<void> {
+    this.loadData(this.state.searchTerm);
+  }
+
+  loadData = async (term: string) => {
+    this.setState({isLoading: true, error: null});
+    try{
+      const data = term ? [await getPokemonByName(term)] : await getPokemonList(10, 0);
+      this.setState({results: data, isLoading: false});
+    } catch (err) {
+      const message = err instanceof Error ? err.message : 'Unknown error';
+      this.setState({
+        results: [],
+        isLoading: false,
+        error: `Could not load data: ${message}`,
+      });
+    }
+    
+    
+  }
+
+  handleSearch = (term: string) => {
+    if(term !== this.state.searchTerm){
+      localStorage.setItem(SEARCH_KEY, term)
+      this.setState({searchTerm: term});
+      this.loadData(term);
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchBar initialTerm={this.state.searchTerm} onSearch={this.handleSearch}></SearchBar>
+        <CardsList></CardsList>
+        {this.state.results.map(item => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+
+      </div>
+
+    )
+  }
+
 }
 
 export default App
