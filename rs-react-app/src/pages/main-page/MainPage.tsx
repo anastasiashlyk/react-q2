0f13c580ec69af3001e4976937c0fad7e90dc4b7
Anastasia Shlyk <shlykk.anastasia@gmail.com>
2026-05-18 22:34:34 +0200

    feat: add main page

---
+++ rs-react-app/src/pages/main-page/MainPage.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import SearchBar from '../../components/SearchBar/SearchBar';
+import CardsList from '../../components/CardsList/CardsList';
+import Pagination from '../../components/Pagination/Pagination';
+import ErrorButton from '../../components/ErrorButton/ErrorButton';
+
+import { SEARCH_KEY, PAGE_SIZE } from '../../constants/consts';
+
+import useLocalStorage from '../../hooks/useLocalStorage.ts';
+import usePokemon from '../../hooks/usePokemon.ts';
+
+function MainPage() {
+  const [searchTerm, setSearchTerm] = useLocalStorage<string>(SEARCH_KEY, '');
+  const [page, setPage] = useState(1);
+  const { results, total, isLoading, error } = usePokemon(searchTerm, page);
+
+  function handleSearch(term: string) {
+    if (term !== searchTerm) {
+      setSearchTerm(term);
+      setPage(1);
+    }
+  }
+
+  function handlePageChange(newPage: number) {
+    setPage(newPage);
+  }
+
+  return (
+    <div>
+      <SearchBar initialTerm={searchTerm} onSearch={handleSearch}></SearchBar>
+      <CardsList
+        results={results}
+        isLoading={isLoading}
+        error={error}
+      ></CardsList>
+      <Pagination
+        page={page}
+        totalPages={Math.ceil(total / PAGE_SIZE)}
+        onPageChange={handlePageChange}
+      />
+      <ErrorButton>Throw Error</ErrorButton>
+    </div>
+  );
+}
+
+export default MainPage;
