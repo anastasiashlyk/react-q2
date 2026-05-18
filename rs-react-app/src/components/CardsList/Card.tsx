e029aa910a797f5df087502d7e49324975ce6b64
Anastasia Shlyk <shlykk.anastasia@gmail.com>
2026-05-18 23:52:41 +0200

    fix: click on card

--- rs-react-app/src/components/CardsList/Card.tsx
+++ rs-react-app/src/components/CardsList/Card.tsx
@@ -1,13 +1,20 @@
 import { CardInfo } from '../../types/pokeapi_types';
+import { useNavigate } from 'react-router-dom';
 import './index.css';
 
 type CardProps = {
   info: CardInfo;
+  currentPage: number;
 };
 
 function Card(props: CardProps) {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate(`/details/${props.info.name}?page=${props.currentPage}`);
+  }
   return (
-    <div className="card">
+    <div className="card" onClick={handleClick}>
       {props.info.image && <img src={props.info.image} alt={props.info.name} />}
       <h3>Name: {props.info.name}</h3>
       <p>Height: {props.info.height}</p>
