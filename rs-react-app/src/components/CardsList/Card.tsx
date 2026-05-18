f92e76821845c71322b6d3232bd2cb4f48a45190
Anastasia Shlyk <shlykk.anastasia@gmail.com>
2026-05-18 14:12:11 +0200

    fix: error btn as functional component

--- rs-react-app/src/components/CardsList/Card.tsx
+++ rs-react-app/src/components/CardsList/Card.tsx
@@ -5,17 +5,15 @@ type CardProps = {
   info: CardInfo;
 };
 
-function Card (props: CardProps){
+function Card(props: CardProps) {
   return (
-      <div className="card">
-        {props.info.image && (
-          <img src={props.info.image} alt={props.info.name} />
-        )}
-        <h3>Name: {props.info.name}</h3>
-        <p>Height: {props.info.height}</p>
-        <p>Weight: {props.info.weight}</p>
-      </div>
-    );
+    <div className="card">
+      {props.info.image && <img src={props.info.image} alt={props.info.name} />}
+      <h3>Name: {props.info.name}</h3>
+      <p>Height: {props.info.height}</p>
+      <p>Weight: {props.info.weight}</p>
+    </div>
+  );
 }
 
 export default Card;
