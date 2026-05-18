4f10e48d8933acd3635c312a678d174ea5636b97
Anastasia Shlyk <shlykk.anastasia@gmail.com>
2026-05-18 22:34:52 +0200

    feat. add about page

---
+++ rs-react-app/src/pages/about-page/AboutPage.tsx
@@ -0,0 +1,18 @@
+function AboutPage() {
+  return (
+    <div>
+      <div className="about">
+        <p>
+          Hiii!! I'm the author of this website. You can find my github
+          here:{' '}
+        </p>
+        <a href="https://github.com/anastasiashlyk">@anastasiashlyk</a>
+      </div>
+      <div>
+        <a href="https://rs.school/courses/reactjs">RS School</a>
+      </div>
+    </div>
+  );
+}
+
+export default AboutPage;
