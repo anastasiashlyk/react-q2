e2cacb4a30292e3d5035350ea40a9b795870c0f1
Anastasia Shlyk <shlykk.anastasia@gmail.com>
2026-05-24 21:37:57 +0200

    feat: add checkbox to cards

--- rs-react-app/src/components/CardsList/Card.tsx
+++ rs-react-app/src/components/CardsList/Card.tsx
@@ -1,5 +1,8 @@
 import { CardInfo } from '../../types/pokeapi_types';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { addItem, removeItem } from '../../store/selectedItemsSlice';
+import { RootState } from '../../store/store';
 import './index.css';
 
 type CardProps = {
@@ -9,12 +12,32 @@ type CardProps = {
 
 function Card(props: CardProps) {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const isSelected = useSelector((state: RootState) =>
+    state.selectedItems.some((item: CardInfo) => item.id === props.info.id)
+  );
 
   function handleClick() {
     navigate(`/details/${props.info.name}?page=${props.currentPage}`);
   }
+
+  function handleCheckbox(e: React.ChangeEvent<HTMLInputElement>) {
+    e.stopPropagation();
+    if (e.target.checked) {
+      dispatch(addItem(props.info));
+    } else {
+      dispatch(removeItem(props.info.id));
+    }
+  }
+
   return (
     <div className="card" onClick={handleClick}>
+      <input
+        type="checkbox"
+        checked={isSelected}
+        onChange={handleCheckbox}
+        onClick={(e) => e.stopPropagation()}
+      />
       {props.info.image && <img src={props.info.image} alt={props.info.name} />}
       <h3>Name: {props.info.name}</h3>
       <p>Height: {props.info.height}</p>
--- rs-react-app/src/store/store.ts
+++ rs-react-app/src/store/store.ts
@@ -6,3 +6,6 @@ export const store = configureStore({
     selectedItems: selectedItemsReducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
